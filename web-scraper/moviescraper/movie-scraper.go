0f9d4d68254bb2218725295c273ec6625841ad43
Emilio Cliff <emiliocliff@gmail.com>
2024-03-12 10:56:47 +0300

    scrapper update

--- web-scraper/moviescraper/movie-scraper.go
+++ web-scraper/moviescraper/movie-scraper.go
@@ -1,31 +1,38 @@
 package main
 
 import (
-	"sync"
 	"encoding/csv"
-	"log"
 	"fmt"
-	"github.com/gocolly/colly"
+	"log"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/gocolly/colly"
 )
 
-type Movie struct{
-	Genre string
-	Title string
-	MovieLink string
-	Year string
-	ImageURL string
+type Movie struct {
+	Genre         string
+	Title         string
+	MovieLink     string
+	Year          string
+	ImageURL      string
 	QualityLevels string
 }
 
-var URL string = "https://www.goojara.to/watch-movies-genre"
+const (
+	URL         = "https://www.goojara.to/watch-movies-genre"
+	scraperType = "without"
+)
 
-func main(){
-	file, err := os.Create("new-movies.csv")
-	if err != nil{
+var elapsedTime time.Duration
+
+func main() {
+	file, err := os.Create("movies.csv")
+	if err != nil {
 		fmt.Println("Error while creating file: ", err)
 	}
-	defer file.Close()c
+	defer file.Close()
 
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
@@ -39,38 +46,69 @@ func main(){
 		"ImageURL",
 	})
 
-	var wg sync.WaitGroup
-
 	genres := []string{"Action", "Adventure", "Comedy", "Drama", "Sci-Fi", "Horror", "Crime", "Thriller", "Romance", "Fantasy"}
+
+	var wg sync.WaitGroup
 	ch := make(chan []Movie, len(genres))
 
-	for _, genre := range genres{
-		wg.Add(1)
-		go scrapeMovies(&wg, genre, ch)
-	}
+	startTime := time.Now()
 
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-
-	for movieList := range ch{
-		for _, movie := range movieList{
-			if err := writer.Write([]string{
-				movie.Genre,
-				movie.Title,
-				movie.Year,
-				movie.QualityLevels,
-				movie.MovieLink,
-				movie.ImageURL,
-			}); err != nil{
-				fmt.Println("Error writing data to CSV")
-			}
+	switch scraperType {
+	case "with":
+		for _, genre := range genres {
+			wg.Add(1)
+			go scrapeMoviesWithGoroutines(&wg, genre, ch)
 		}
+
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
+
+		elapsedTime = time.Since(startTime)
+
+		for movieList := range ch {
+			writeMoviesToCSV(writer, movieList)
+		}
+
+	case "without":
+		var allMovieList [][]Movie
+		for _, genre := range genres {
+			returnMovieList := scrapeMoviesWithoutGoroutines(genre)
+			allMovieList = append(allMovieList, returnMovieList)
+		}
+
+		elapsedTime = time.Since(startTime)
+
+		for _, movieList := range allMovieList {
+			writeMoviesToCSV(writer, movieList)
+		}
+
+	default:
+		fmt.Println("Invalid input. Please enter 'with' or 'without'.")
 	}
+
+	fmt.Printf("Time Taken: %v\n", elapsedTime)
+
 }
 
-func scrapeMovies(wg *sync.WaitGroup, genre string, ch chan<- []Movie){
+func writeMoviesToCSV(writer *csv.Writer, movieList []Movie) {
+	for _, movie := range movieList {
+		if err := writer.Write([]string{
+			movie.Genre,
+			movie.Title,
+			movie.Year,
+			movie.QualityLevels,
+			movie.MovieLink,
+			movie.ImageURL,
+		}); err != nil {
+			fmt.Println("Error writing data to CSV")
+		}
+	}
+
+}
+
+func scrapeMoviesWithGoroutines(wg *sync.WaitGroup, genre string, ch chan<- []Movie) {
 	defer wg.Done()
 
 	c := colly.NewCollector()
@@ -79,9 +117,9 @@ func scrapeMovies(wg *sync.WaitGroup, genre string, ch chan<- []Movie){
 
 	newURL := fmt.Sprintf("%s-%s", URL, genre)
 
-	c.OnHTML("div.dflex", func(e *colly.HTMLElement){
+	c.OnHTML("div.dflex", func(e *colly.HTMLElement) {
 
-		e.ForEach("div > a", func(_ int, a *colly.HTMLElement){
+		e.ForEach("div > a", func(_ int, a *colly.HTMLElement) {
 			var movie Movie
 			movie.Genre = genre
 			movie.Title = a.ChildText("span.mtl")
@@ -93,14 +131,47 @@ func scrapeMovies(wg *sync.WaitGroup, genre string, ch chan<- []Movie){
 		})
 	})
 
-	c.OnRequest(func(r *colly.Request){
+	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting: ", r.URL)
 	})
 
 	err := c.Visit(newURL)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	ch <- movieList
+}
+
+func scrapeMoviesWithoutGoroutines(genre string) []Movie {
+	c := colly.NewCollector()
+
+	movieList := []Movie{}
+
+	newURL := fmt.Sprintf("%s-%s", URL, genre)
+
+	c.OnHTML("div.dflex", func(e *colly.HTMLElement) {
+
+		e.ForEach("div > a", func(_ int, a *colly.HTMLElement) {
+			var movie Movie
+			movie.Genre = genre
+			movie.Title = a.ChildText("span.mtl")
+			movie.MovieLink = a.Attr("href")
+			movie.ImageURL = a.ChildAttr("img", "src")
+			movie.QualityLevels = a.ChildText("span.hda")
+			movie.Year = a.ChildText("span.hdy")
+			movieList = append(movieList, movie)
+		})
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL)
+	})
+
+	err := c.Visit(newURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return movieList
 }
