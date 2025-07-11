0d5c79df016dd2d07af1ad120cbed6495a72529d
devops <devops@Lamahs-MacBook-Pro.local>
2025-07-11 07:46:28 +0300

    request with SOAP api

---
+++ requests/go.mod
@@ -0,0 +1,5 @@
+module github.com/EmilioCliff/learn-go/requests
+
+go 1.24.4
+
+require github.com/hooklift/gowsdl v0.5.0 // indirect
---
+++ requests/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/EmilioCliff/learn-go/requests/myservice"
+	"github.com/hooklift/gowsdl/soap"
+)
+
+func main() {
+	countrySeviceClient := soap.NewClient("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso")
+	countryService := myservice.NewCountryInfoServiceSoapType(countrySeviceClient)
+
+	capitalCityName, err := countryService.CapitalCity(&myservice.CapitalCity{SCountryISOCode: "KE"})
+	if err != nil {
+		log.Fatalf("Error calling CapitalCity: %v", err)
+	}
+	log.Println(capitalCityName.CapitalCityResult)
+
+	fullCountryInfoRequest := &myservice.CountriesUsingCurrency{SISOCurrencyCode: "KES"}
+	fullCountryInfoResponse, err := countryService.CountriesUsingCurrency(fullCountryInfoRequest)
+	if err != nil {
+		log.Fatalf("Error calling FullCountryInfo: %v", err)
+	}
+
+	for i := range fullCountryInfoResponse.CountriesUsingCurrencyResult.TCountryCodeAndName {
+		fmt.Printf("Country Code: %s, Country Name: %s\n",
+			fullCountryInfoResponse.CountriesUsingCurrencyResult.TCountryCodeAndName[i].SISOCode,
+			fullCountryInfoResponse.CountriesUsingCurrencyResult.TCountryCodeAndName[i].SName)
+	}
+}
